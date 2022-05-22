0b8cd2857376f950269db54ec5d894977300d860
Vladimir Makarov <gerbilin@yandex.ru>
2022-05-22 07:54:43 +0300

    Initial Commit

---
+++ HW12-Pomo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12-Pomo
+//
+//  Created by Gerbilin Mac on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
